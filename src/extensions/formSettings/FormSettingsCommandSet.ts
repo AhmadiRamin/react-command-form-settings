c14ea2ccbe953f68329526f2885f2da231b4e60e
Ramin Ahmadi <ramin.ahmadi@live.com>
2019-06-30 11:33:43 +0100

    Added comments

--- src/extensions/formSettings/FormSettingsCommandSet.ts
+++ src/extensions/formSettings/FormSettingsCommandSet.ts
@@ -24,8 +24,8 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
   private listService = new ListService();
   private formSettings: IFormItem[] = [];
   private contentTypes: any[] = [];
-  private ovverideEditClick = false;
-  private ovverideDispClick = false;
+  private overrideEditClick = false;
+  private overrideDispClick = false;
   private editForms: IFormItem[];
   private displayForms: IFormItem[];
   private selectedRow = null;
@@ -40,24 +40,30 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
   @override
   public async onListViewUpdated(event: IListViewCommandSetListViewUpdatedParameters): Promise<void> {
     const listId = String(this.context.pageContext.list.id);
+    // Retrive form settings for current list
     if (this.formSettings.length <= 0)
       this.formSettings = await this.listService.getEnabledFormSettings(listId);
-
+    // Get available content types
     if (this.contentTypes.length <= 0)
       this.contentTypes = await this.listService.getListContentTypes(listId);
 
+    // Override New buttons' click event
     const newForms = this.formSettings.filter(i => i.FormType === "New");
-    this.ovverideNewFormSettings(newForms, this.contentTypes.length);
+    this.overrideNewFormSettings(newForms, this.contentTypes.length);
 
+    // When users click on Title this method is not fired, so workground is to select the row to trigger this event
     $("body").on("click", `button[data-automationid='FieldRenderer-title']`, (e) => {
       this.selectedRow = $(e.target).parents().closest("div[data-automationid='DetailsRow']");
       this.selectedRow.trigger("click");
     });
 
     if (event.selectedRows.length > 0) {
+      // Get item Id to be replaced in with {ItemId} token
       this.itemId = event.selectedRows[0].getValueByName("ID");
 
+      // If we have more than one content type, we need to filter the settings based on content types to override the events
       if (this.contentTypes.length > 1) {
+        // Get content type value, if you have more than one, you have to add Content Type column to the list views
         const contentType = event.selectedRows[0].getValueByName("ContentType");
         this.editForms = this.formSettings.filter(i => i.ContentTypeName === contentType && i.FormType === "Edit");
         this.displayForms = this.formSettings.filter(i => i.ContentTypeName === contentType && i.FormType === "Display");
@@ -67,31 +73,39 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
         this.displayForms = this.formSettings.filter(i => i.FormType === "Display");
       }
 
+      // Override Edit button's click event
       if (this.editForms.length > 0) {
-        this.ovverideEditClick = true;
+        // This flag helps to not override other content types which don't have settings
+        this.overrideEditClick = true;
         this.overrideOnClick("Edit", this.editForms[0]);
       }
       else {
-        this.ovverideEditClick = false;
+        this.overrideEditClick = false;
       }
+      // Override Open button's click event
       if (this.displayForms.length > 0) {
-        this.ovverideDispClick = true;
+        this.overrideDispClick = true;
         this.overrideOnClick("Open", this.displayForms[0]);
+        // If user clicks on Title field, we trigger the overrided event
         if (this.selectedRow) {
           this.selectedRow = null;
           this.redirect(this.displayForms[0]);
+          // To close the Display panel
           $(document.body.firstChild).trigger("click");
 
         }
       }
       else {
         this.selectedRow = null;
-        this.ovverideDispClick = false;
+        this.overrideDispClick = false;
       }
 
     }
   }
 
+  /**
+    * Open settings panel component
+  */
   @override
   public async onExecute(event: IListViewCommandSetExecuteEventParameters): Promise<void> {
     if (event.itemId === "COMMAND_Form_Settings") {
@@ -109,8 +123,11 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
 
     }
   }
-  private async ovverideNewFormSettings(formSettings: IFormItem[], ctCount: number) {
-    // Ovveride if only one content type exists in the list
+  /**
+    * Override new form settings
+  */
+  private async overrideNewFormSettings(formSettings: IFormItem[], ctCount: number) {
+    // override if only one content type exists in the list
     if (ctCount < 2)
       this.overrideOnClick("New", formSettings[0]);
     else {
@@ -119,20 +136,24 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
       });
     }
   }
-
+  /**
+    * Override buttons' event
+  */
   private overrideOnClick(tagName: string, settings: IFormItem) {
     $("body").on("click", `button[name='${tagName}']`, (e) => {
       switch (tagName) {
         case "Edit":
-          this.ovverideEditClick && this.redirect(settings); return e.stopPropagation();
+          this.overrideEditClick && this.redirect(settings); return e.stopPropagation();
         case "Open":
-          this.ovverideDispClick && this.redirect(settings); return e.stopPropagation();
+          this.overrideDispClick && this.redirect(settings); return e.stopPropagation();
         default:
           this.redirect(settings); return e.stopPropagation();
       }
     });
   }
-
+  /**
+    * Redirect window based on form settings
+  */
   private redirect(settings: IFormItem) {
     const { OpenIn, RedirectURL, Parameters } = settings;
     let tokens = "";
@@ -146,7 +167,9 @@ export default class FormsSettingsCommandSet extends BaseListViewCommandSet<IFor
         break;
     }
   }
-
+  /**
+    * Replace tokens
+  */
   private replaceTokens(tokens: string) {
     if (!tokens)
       return "";
